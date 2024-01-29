b0d0c102c46d070df9d85a5d4592c00527223d4b
Youssef ElGohre <118435243+MEyoussef@users.noreply.github.com>
2024-01-29 07:58:07 -0800

    Full Source

--- GuessingGame.cpp
+++ GuessingGame.cpp
@@ -2,37 +2,84 @@
 #include <string>
 int main()
 {
-    std::cout << "\n\n=======================\n\n";
-    std::cout << "=== Guessing Game Game ===" << std::endl;
-    std::cout << "\n=======================\n\n";
+    std::cout << std::endl;
+    std::cout << "== Casino Guess Game ==" << std::endl
+              << std::endl;
+    std::cout << "- GAME RULES -" << std::endl;
+    std::cout << "IF YOU WON YOU GET 10X TIMES YOUR BET" << std::endl;
+    std::cout << "IF YOU LOSE, YOU LOSE ALL YOUR BET" << std::endl;
 
     std::string Player_Name = "";
-    std::cout << "Enter Your Name : ";
+    int Player_Balance = 0, Player_Bet = 0, Player_Guess;
+    char Play_Again;
+
+    srand(time(0));
+    int Computer_Number = rand() % (1 + 3 - 1) + 1;
+
+    std::cout << "Your Name : ";
     std::cin >> Player_Name;
 
-    int Player_Balance = 0;
-    std::cout << "Enter Your Balance : "
-              << "$";
+    std::cout << std::endl;
+
+    std::cout << "What Is Your Balance : ";
     std::cin >> Player_Balance;
 
-    int Initial_Amount = 0;
-    std::cout << "Enter Your Bet : "
-              << "$";
-    std::cin >> Initial_Amount;
+    std::cout << std::endl;
 
     std::cout << std::endl;
 
-    std::cout << "########" << std::endl;
-    std::cout << "Player Name Is : " << Player_Name << std::endl;
-    std::cout << "Player Bet Is : " << Initial_Amount << std::endl;
-    std::cout << "Your Balance Is : " << Player_Balance - Initial_Amount << std::endl;
-    std::cout << "########" << std::endl;
+    do
+    {
+        // Get Player's Betting Amount
 
-    std::cout << std::endl;
+        do
+        {
+            std::cout << "Enter Bet : ";
+            std::cin >> Player_Bet;
 
-    std::cout << "RULES OF THE GAME" << std::endl;
-    std::cout << "1- Choose Any Number Between 1 To 10" << std::endl;
-    std::cout << "2- If You Win You Will Get 10 Times Of Money You Bet" << std::endl;
-    std::cout << "3- If Your Bet on Wrong Number You Will Lose Your Betting Amount" << std::endl;
+            if (Player_Bet > Player_Balance)
+            {
+                std::cout << std::endl;
+                std::cout << "Your Bet > Your Balance. ReEnter Data";
+            }
+        } while (Player_Bet > Player_Balance);
+
+        // Get Player's Guess
+        do
+        {
+            std::cout << "Guess Winning Number [ 1 - 3 ]: ";
+            std::cin >> Player_Guess;
+
+            if (Player_Guess <= 0 || Player_Guess > 3)
+            {
+                std::cout << "Please Enter Valid Data" << std::endl;
+            }
+        } while (Player_Guess <= 0 || Player_Guess > 3);
+
+        if (Computer_Number == Player_Guess)
+        {
+
+            std::cout << "Congrats ! You Won : " << Player_Bet * 10 << std::endl;
+            Player_Balance = Player_Balance + (Player_Bet * 10);
+        }
+        else
+        {
+            std::cout << "Good Luck Next Time, You Lost : " << Player_Bet << std::endl;
+            Player_Balance = Player_Balance - Player_Bet;
+        }
+        std::cout << "The Winning Number Was " << Computer_Number << std::endl;
+        std::cout << Player_Name << " You Have " << Player_Balance << std::endl;
+
+        if (Player_Balance == 0)
+        {
+            std::cout << "You Have No Balance To Play" << std::endl;
+            break;
+        }
+
+        std::cout << "Do You Want To Play Again [ Y / N ] : ";
+        std::cin >> Play_Again;
+    } while (Play_Again == 'Y' || Play_Again == 'y');
+
+    std::cout << "Thank You For Playing Your Balance Is " << Player_Balance << std::endl;
     return 0;
 }
